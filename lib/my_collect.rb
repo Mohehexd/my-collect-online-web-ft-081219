969d333559f6eff8c4207b7b8979c336cefe8c8f
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-24 19:10:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,11 @@
 def my_collect(array)
   collection = []
   i = 0 
-  while i < array.length do |language|
+    while i < array.length do |language|
   collection << yield language.upcase
   i += 1 
-  end 
+    end 
   collection 
+  end
 end 
 
